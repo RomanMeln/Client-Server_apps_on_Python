f20472e0670ba6abf2c3ca7e9095c5816ed5c4b9
roman <tupozarabotok>
2022-05-07 20:46:36 +0300

    Lesson-3. Fixes

--- HW #3/client.py
+++ HW #3/client.py
@@ -3,27 +3,21 @@ import time
 from socket import *
 import sys
 import json
+from utils import send_message, get_message
+
 
 ACTIONS = "presence"
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 DEFAULT_PORT = 7777
 
-# s = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
-# s.connect(('localhost', DEFAULT_PORT)) # Соединиться с сервером
-# msg = {
-#     "action": ACTIONS,
-#     "time": time.time(),
-#     }
-#
-#
-# s.send(json.dumps(msg).encode('utf-8')) # отправка сообщения json в строке с кодировкой
-# data = s.recv(1000000) # прием данных от сервера в байтах
-# data_utf = data.decode('utf-8') # декодирование данных с сервера
-# print(type(data_utf))
-# data_json = json.loads(data_utf) # перевод данных из строки в словарь
-# print(type(data_json))
-# print('Сообщение от сервера: ', data_json)
-# s.close() # закрыть соединение
+
+def msg_for_server():
+    message = {
+        "action": ACTIONS,
+        "time": time.time(),
+    }
+    return message
+
 
 def main():
     """Загружаем параметы коммандной строки"""
@@ -39,19 +33,14 @@ def main():
         print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
         sys.exit(1)
 
-    s = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
-    s.connect((server_address, server_port))  # Соединиться с сервером
-    msg = {
-        "action": ACTIONS,
-        "time": time.time(),
-    }
-    s.send(json.dumps(msg).encode('utf-8'))  # отправка сообщения json в строке с кодировкой
+    client_socket = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
+    client_socket.connect((server_address, server_port))  # Соединиться с сервером
+    message = msg_for_server() # создание сообщения
+    send_message(client_socket, message)  # используем функцию для отправки сообщения
+
     try:
-        data = s.recv(1000000)  # прием данных от сервера в байтах
-        data_utf = data.decode('utf-8')  # декодирование данных с сервера
-        data_json = json.loads(data_utf)  # перевод данных из строки в словарь
-        print('Сообщение от сервера: ', data_json)
-        s.close()  # закрыть соединение
+        answer = get_message(client_socket) # получаем сообщение
+        print('Сообщение от сервера: ', answer['response'], ':', answer['alert']) # выводим ответ
     except (ValueError, json.JSONDecodeError):
         print('Не удалось декодировать сообщение сервера.')
 
--- HW #3/server.py
+++ HW #3/server.py
@@ -1,50 +1,39 @@
 # Программа сервера для получения приветствия от клиента и отправки ответа
 from socket import *
-import time
 import json
 import sys
+from utils import send_message, get_message
 
 DEFAULT_PORT = 7777
 MAX_CONNECTIONS = 5
 
 
-# s = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
-# s.bind(('', DEFAULT_PORT)) # Присваивает порт 7777
-# s.listen(5) # Переходит в режим ожидания запросов;
-#             # Одновременно обслуживает не более
-#             # 5 запросов.
+def answer_from_server(responses):
+    """
+    Функция формирования сообщения от сервера
+    """
 
-# answer = ''
-# alert = 'Успешно'
-# while True:
-#     client, addr = s.accept() # Принять запрос на соединение
-#     print(client)
-#     print(addr)
-#     print('-*-' * 20)
-#     data = client.recv(1000000) # прием данных от клиента
-#     print(data)
-#     print(type(data))
-#     print('-**-' * 15)
-#     data_utf = data.decode('utf-8')
-#     print(type(data_utf))
-#     print('-***-' * 11)
-#     data_json = json.loads(data_utf)
-#     print(data_json)
-#     print('-****-' * 8)
-#     if data_json['action'] == 'presence':
-#         answer = '200'
-#     else:
-#         answer = '400'
-#         alert = 'Bad Request'
-#
-#     answer_msg = {
-#         "response": answer,
-#         "alert": alert,
-#         }
-#
-#     # print('Сообщение: ', data, ', было отправлено клиентом: ', addr)
-#     client.send(json.dumps(answer_msg).encode('utf-8')) # отправка сообщения клиенту
-#     client.close() # закрытие соединения
+    msg = {
+        "response": responses[0],
+        "alert": responses[1],
+    }
+    return msg
+
+
+def choice_response(msg):
+    """
+    Функция выбора соответствующего
+    ответа на сообщение от клиента
+    """
+
+    if msg['action'] == 'presence':
+        answer = '200'
+        alert = 'Succesful'
+    else:
+        answer = '400'
+        alert = 'Bad Request'
+    return answer, alert
+
 
 def main():
     """
@@ -78,33 +67,19 @@ def main():
             'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
         sys.exit(1)
 
-    s = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
-    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    s.bind((listen_address, listen_port)) # Присваивает порт указанный в параметре либо DEFAULT_PORT
-    s.listen(MAX_CONNECTIONS) # Переходит в режим ожидания запросов;
+    socket_server = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
+    socket_server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    socket_server.bind((listen_address, listen_port)) # Присваивает порт указанный в параметре либо DEFAULT_PORT
+    socket_server.listen(MAX_CONNECTIONS) # Переходит в режим ожидания запросов;
             # Одновременно обслуживает не более MAX_CONNECTIONS запросов
 
     while True:
-        client, addr = s.accept()
+        client, addr = socket_server.accept()
         try:
-            data = client.recv(1000000) # прием данных от клиента
-            data_utf = data.decode('utf-8') # декодирование байтов в кодировку utf-8
-            data_json = json.loads(data_utf) # загрузка словаря json
-
-            if data_json['action'] == 'presence':
-                answer = '200'
-                alert = 'Succesful'
-            else:
-                answer = '400'
-                alert = 'Bad Request'
-
-            answer_msg = {
-                "response": answer,
-                "alert": alert,
-            }
-
-            # print('Сообщение: ', data, ', было отправлено клиентом: ', addr)
-            client.send(json.dumps(answer_msg).encode('utf-8'))  # отправка сообщения клиенту
+            data = get_message(client)
+            response_msg = choice_response(data) # получаем кортеж данных для ответа
+            answer_msg = answer_from_server(response_msg) # формируем ответ для клиента
+            send_message(client, answer_msg) # используем функцию для отправки сообщения
             client.close()
         except(ValueError, json.JSONDecodeError):
             print('Принято некорректное сообщение от клиента.')
